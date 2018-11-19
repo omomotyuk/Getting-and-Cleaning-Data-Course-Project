45fab5813897803d489cd77f322e2d79cb8c1a71
omomotyuk <43194659+omomotyuk@users.noreply.github.com>
2018-11-18 22:20:59 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,48 @@
 ##
-## Download and unzip data files.
+## 1. Download and unzip data files.
 ##
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file( fileUrl,destfile="data/getdata_projectfiles_UCI HAR Dataset.zip" )
+download.file( fileUrl,destfile="getdata_projectfiles_UCI HAR Dataset.zip" )
+
+system( "unzip getdata_projectfiles_UCI\\ HAR\\ Dataset.zip" )
 
 ##
-## Merges the training and the test sets to create one data set.
+##  2. Merges the training and the test sets to create one data set.
 ##
 
-X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 
 data_set <- rbind( X_train,X_test )
 
 ##
-## Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Extracts only the measurements on the mean and standard deviation for each measurement.
 ##
 
-features <- read.table("./data/UCI HAR Dataset/features.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 names( data_set ) <- features[ ,2 ]
 data_set <- data_set[ grepl( "-mean\\(|-std\\(" ,names(data_set) ) ]
 
 ##
-## Uses descriptive activity names to name the activities in the data set.
-## Appropriately labels the data set with descriptive variable names.
+## 4. Uses descriptive activity names to name the activities in the data set and 
+##    appropriately labels the data set with descriptive variable names.
 ##
 
-subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 subject_set <- rbind( subject_train,subject_test )
 
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
 y_set <- rbind( y_train,y_test )
 
 subject_activity_set <- cbind( subject_set,y_set )
 names( subject_activity_set ) <- c( "Subject","Activity" )
 
-activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 names( activity_labels ) <- c( "Activity","Activity.labels" )
 
 subject_activity_set <- merge( subject_activity_set,activity_labels,by.x="Activity",by.y="Activity" )
@@ -48,12 +50,28 @@ subject_activity_set <- merge( subject_activity_set,activity_labels,by.x="Activi
 data_set <- cbind( subject_activity_set,data_set )
 
 ##
-## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 ##
 
 library( dplyr )
 
 data_tbl <- tbl_df( data_set )
 
-group_by( data_tbl, Subject, Activity.labels ) %>%
-   summarise_all( funs( mean ) ) 
+data_tbl <- select( data_tbl,-Activity )
+data_tbl <- rename( data_tbl,Activity = Activity.labels )
+
+tidyData <- function( data ){
+group_by( data, Subject, Activity ) %>%
+   summarise_all( funs( mean ) )
+}
+
+tidyDataSet <- tidyData( data_tbl )
+
+##
+## 6. Writes tidy data set in tidyDataSet.csv file and prepares draft CodeBook.md file.
+##
+
+write.csv( tidyDataSet,file="tidyDataSet.csv",row.names = FALSE)
+write.table( names(tidyDataSet),file="addToCodeBook.txt",quote = FALSE,sep = "\t" )
+
+system( "cat ./UCI\\ HAR\\ Dataset/README.txt ./UCI\\ HAR\\ Dataset/features_info.txt ./UCI\\ HAR\\ Dataset/activity_labels.txt addToCodeBook.txt > CodeBook.md" )
